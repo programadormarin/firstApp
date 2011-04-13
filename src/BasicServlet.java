bc5f4c1acd1f99672829298c4ffcb0ebb43991d3
Hermengildo Marin Júnior <programador.marin@gmail.com>
2011-04-12 21:55:11 -0300

    1º

---
+++ src/BasicServlet.java
@@ -0,0 +1,27 @@
+
+
+import java.io.IOException;
+import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class BasicServlet
+ */
+public class BasicServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {		
+		ServletOutputStream out = response.getOutputStream();
+		response.setContentType("text/html");
+		out.println("<html><head><title>Basic Servlet </title></head>");
+		out.println("<body>Hello World</body></html>");
+	}
+
+
+}
